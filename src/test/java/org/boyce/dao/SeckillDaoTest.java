2e1691af452e0dc2b731f1711ac6a8ce452570bc
Boyce <Aaron.Ngen@outlook.com>
2017-12-03 21:04:02 +0800

    update

--- src/test/java/org/boyce/dao/SeckillDaoTest.java
+++ src/test/java/org/boyce/dao/SeckillDaoTest.java
@@ -14,6 +14,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 /**
  * 配置Spring和Junit整合,junit启动时加载springIOC容器 spring-test,junit
  */
+
 @RunWith(SpringJUnit4ClassRunner.class)
 // 告诉junit spring的配置文件
 @ContextConfiguration({ "classpath:spring/spring-dao.xml" })
