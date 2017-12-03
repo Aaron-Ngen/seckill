d7aef0a733a2244ab5e9a2273cc8d5fe35167744
Boyce <Aaron.Ngen@outlook.com>
2017-12-03 21:00:45 +0800

    update

---
+++ src/test/java/org/boyce/dao/cache/RedisDaoTest.java
@@ -0,0 +1,43 @@
+package org.boyce.dao.cache;
+
+import org.boyce.entity.Seckill;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import org.boyce.dao.SeckillDao;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+// 告诉junit spring的配置文件
+@ContextConfiguration({ "classpath:spring/spring-dao.xml" })
+public class RedisDaoTest {
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	private long id = 1001;
+
+	@Autowired
+	private RedisDao redisDao;
+
+	@Autowired
+	private SeckillDao seckillDao;
+
+	@Test
+	public void testSeckill() {
+
+		Seckill seckill = redisDao.getSeckill(id);
+		if (seckill == null) {
+			seckill = seckillDao.queryById(id);
+			if (seckill != null) {
+				String result = redisDao.putSeckill(seckill);
+				logger.info("result={}", result);
+				seckill = redisDao.getSeckill(id);
+				logger.info("seckill={}", seckill);
+			}
+		}
+	}
+
+}
